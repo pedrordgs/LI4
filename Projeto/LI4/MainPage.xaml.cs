a18a95ea9f7148e11d5ea27cea8f16f5601888f0
Pedro Rodrigues <a84783@uminho.pt>
2020-03-01 00:01:00 +0000

    First Page background added

---
+++ Projeto/LI4/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LI4
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"Entraste {count} vezes.";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"Registaste {count} vezes.";
+        }
+    }
+}
