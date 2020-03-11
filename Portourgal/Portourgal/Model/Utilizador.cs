4e5a9ceed3c8a5c17eda68ab6843ecd2341459d3
Pedro Rodrigues <a84783@uminho.pt>
2020-03-11 18:47:43 +0000

    Connected to database

--- Portourgal/Portourgal/Model/Utilizador.cs
+++ Portourgal/Portourgal/Model/Utilizador.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,35 +8,37 @@ namespace Portourgal.Model
 {
     class Utilizador
     {
-        private String nome;
-        private String cidade;
-        private String distrito;
-        private String email;
-        private String password;
-
         public Utilizador()
         {
-            this.nome = "";
-            this.cidade = "";
-            this.distrito = "";
-            this.email = "";
-            this.password = "";
+            this.Nome = "";
+            this.Cidade = "";
+            this.Distrito = "";
+            this.Email = "";
+            this.Password = "";
         }
 
         public Utilizador(String n, String c, String d, String m, String p)
         {
-            this.nome = n;
-            this.cidade = c;
-            this.distrito = d;
-            this.email = m;
-            this.password = p;
+            this.Nome = n;
+            this.Cidade = c;
+            this.Distrito = d;
+            this.Email = m;
+            this.Password = p;
         }
 
-        public string Nome { get { return nome; } set { nome = value; } }
-        public string Cidade { get { return cidade; } set { cidade = value; } }
-        public string Distrito { get { return distrito; } set { distrito = value; } }
-        public string Email { get { return email; } set { email = value; } }
-        public string Password { get { return password; } set { password = value; } }
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string Distrito { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
+        public void AddToDatabase()
+        {
+            MongoClient client = new MongoClient("mongodb://li4:li4@cluster0-shard-00-00-lscfy.gcp.mongodb.net:27017,cluster0-shard-00-01-lscfy.gcp.mongodb.net:27017,cluster0-shard-00-02-lscfy.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority");
+            var database = client.GetDatabase("PortourgalDB");
+            var collection = database.GetCollection<BsonDocument>("Users");
+            var document = new BsonDocument {{ "nome", Nome }, { "cidade", Cidade }, { "distrito", Distrito }, { "email", Email }, { "password", Password } };
+            collection.InsertOne(document);
+        }
     }
 }
--- Portourgal/Portourgal/ViewModel/RegistarViewModel.cs
+++ Portourgal/Portourgal/ViewModel/RegistarViewModel.cs
@@ -8,31 +8,26 @@ namespace Portourgal.ViewModel
 {
     class RegistarViewModel
     {
-        string nome;
-        string cidade;
-        string distrito;
-        string email;
-        string password;
-
         public RegistarViewModel()
         {
-            this.nome = "";
-            this.cidade = "";
-            this.distrito = "";
-            this.email = "";
-            this.password = "";
+            this.Nome = "";
+            this.Cidade = "";
+            this.Distrito = "";
+            this.Email = "";
+            this.Password = "";
             ComandoRegistar = new Command(RegistarUtilizador);
         }
 
-        public string Nome { get { return nome; } set { nome = value; } }
-        public string Cidade { get { return cidade; } set { cidade = value; } }
-        public string Distrito { get { return distrito; } set { distrito = value; } }
-        public string Email { get { return email; } set { email = value; } }
-        public string Password { get { return password; } set { password = value; } }
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string Distrito { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
         void RegistarUtilizador()
         {
-            new Utilizador(nome, cidade, distrito, email, password);
+            Utilizador u = new Utilizador(Nome, Cidade, Distrito, Email, Password);
+            u.AddToDatabase();
         }
 
         public Command ComandoRegistar { get; }
