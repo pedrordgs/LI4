6afbabcf1f0d2955ed1fa0147a70f101b492d9aa
pedrordgs <pedrombr99@gmail.com>
2020-06-16 14:41:24 +0100

    changed xaml to receive url to images

--- Portourgal/Portourgal/ViewModel/MainContentPageViewModel.cs
+++ Portourgal/Portourgal/ViewModel/MainContentPageViewModel.cs
@@ -14,7 +14,6 @@ namespace Portourgal.ViewModel
         public MainContentPageViewModel()
         {
             Distritos = DistritoInteraction.GetDistritos().Result;
-            Image = new Uri("https://img.itinari.com/pages/images/original/5b608edd-a690-49ba-8adf-3c5c0b44516f-istock-883146964.jpg?ch=DPR&dpr=1&w=1600&s=9d2d5b50305a4f9ffb411b649696d080");
             ComandoDistrito = new Command(SelecionarDistrito);
         }
 
