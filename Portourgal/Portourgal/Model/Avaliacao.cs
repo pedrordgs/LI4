b82b015f160ce2b2d6224e0b7adc37f8605815c3
pedrordgs <pedrombr99@gmail.com>
2020-07-02 17:42:12 +0100

    added classificiation on atractions

---
+++ Portourgal/Portourgal/Model/Avaliacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portourgal.Model
+{
+    public class Avaliacao
+    {
+        public Avaliacao(string e, int c)
+        {
+            Email = e;
+            Classificacao = c;
+        }
+        public string Email { get; set; }
+        public int Classificacao { get; set; }
+    }
+}
